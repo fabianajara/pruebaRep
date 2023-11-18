d37438e3d9db1553f222835c400a9ad8c20fdb0f
Valeospina <rengifovale7@gmail.com>
2023-11-18 17:42:06 -0600

    2 commit

--- src/main/java/com/mycompany/pruebagit/Pruebagit.java
+++ src/main/java/com/mycompany/pruebagit/Pruebagit.java
@@ -11,6 +11,7 @@ package com.mycompany.pruebagit;
 public class Pruebagit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");       
+       System.out.println("Valeria!");
     }
 }
